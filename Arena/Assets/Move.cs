35b9f1feaab85489167374961f71886d22f0f6a3
AVCCompSci <56980171+AVCCompSci@users.noreply.github.com>
2019-10-24 16:51:22 -0700

    made a box move up and down

---
+++ Arena/Assets/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 posistion = transform.position;
+        float newPos = Mathf.Sin(Time.time * .75f);
+        transform.position = new Vector3(.0f, newPos, .0f) * 5f; 
+        
+
+
+    }
+}
