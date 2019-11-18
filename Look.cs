7b7179a19c76b20fe99a849c2b5c33f5f77ea9c1
AVCCompSci <56980171+AVCCompSci@users.noreply.github.com>
2019-11-18 10:22:24 -0800

    Add files via upload

---
+++ Look.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;                                           //reference to gameobject
+    [SerializeField]                                                                
+    
+    private Transform look;                                                 //reference to gameobject
+
+
+    private float speed = 3.5f;
+
+
+    [SerializeField]
+    private Vector2 limits = new Vector2(-75f, 90f);
+
+    [SerializeField]
+    private Vector2 Position;                                        // screen position
+    [SerializeField]
+    private Vector2 currentMouse;                                   //reference to where our mouse would be
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        LookUpandDown();
+    }
+    void LookUpandDown()
+    {
+        currentMouse = new Vector2(Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X")); //2d representation of mouse location
+        Position.x = Mathf.Clamp(Position.x, limits.x, limits.y);                       //keep the value between some limits
+        Position.x = Position.x + currentMouse.x * -speed;                                 //add the mouse inputs to position x
+        Position.y = Position.y + currentMouse.y * speed;                                  //add the mouse inputs to position y
+
+       
+        //rotations
+        look.localRotation = Quaternion.Euler(Position.x, 0, 0f);                      //local rotate the look root
+        player.localRotation = Quaternion.Euler(0, Position.y, 0f);                    //local rotate the player root
+        
+
+
+
+
+
+    }
+}
---
+++ Move.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    private CharacterController Controller;
+   
+    public Vector3 dashDirection;
+    public float dashDistance = 15;
+    public float dashSpeed = 10;
+
+    //member variables for movement
+    public float speed = 5f;
+    public float gravity = 20f;
+    public float Velocity;                                      //you get it
+    public float Force = 9.8f;
+    public Vector3 moveVector;                                  //important variable to store 3 variable for 3 directions(xyz)
+  
+   
+    void Update()
+    {
+        MovePlayer();
+        Debug.Log(Velocity);
+        if (Input.GetButtonDown("Fire2"))
+        {
+          
+                Dash();
+            
+        }
+    }
+    void Dash()
+    {
+        dashDirection = transform.forward * dashSpeed;                          //get the forward vector and make it the direction(forward)         
+        Controller.Move(Time.deltaTime * dashDistance * dashSpeed * dashDirection); //call the character move function and basically move it a big number
+
+    }
+    /*
+    void DashBackWards()
+    {
+        dashDirection = -transform.forward * dashSpeed;                                 
+        Controller.Move(Time.deltaTime * dashDistance * dashSpeed * dashDirection);
+
+    }
+    */
+    void Awake()                                                //get a reference to the character controller on script load
+    {
+        Controller = GetComponent<CharacterController>(); 
+    }
+
+    void MovePlayer()
+    {
+        
+        //declare the move variable 
+        moveVector = new Vector3(Input.GetAxis("Horizontal"),0f,Input.GetAxis("Vertical"));
+
+        moveVector = transform.TransformDirection(moveVector);     //move from local to world space(go in the direction you are looking        
+        moveVector *= speed * Time.deltaTime;                      //make it a constant speed
+        Gravity();
+        Controller.Move(moveVector);                               //move bro                      
+        
+
+    } 
+
+    void Gravity()                                  //add "artificial gravity" so that the controller is touching the ground
+    {
+
+        Velocity -= gravity * Time.deltaTime;      //start making the valocity negative 
+
+        
+        Jump();                              //jump
+
+        moveVector.y = Velocity * Time.deltaTime;   //when spacebar is pressed it launches you into the air, and gravity will bring you down
+    }
+   
+        void Jump()
+        {
+
+            if (Controller.isGrounded && Input.GetKeyDown(KeyCode.Space)) //if you press space bar you
+            {
+                     Velocity = Force;                                         //jump
+            }
+
+        }
+
+} 
+
