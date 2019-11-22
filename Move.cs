14b6aa80b0645e3f43276006fe48156564f3dcf7
AVCCompSci <56980171+AVCCompSci@users.noreply.github.com>
2019-11-22 09:52:24 -0800

    Add files via upload
    updated to handle a cooldown on the dashes

--- Move.cs
+++ Move.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Move : MonoBehaviour
 {
 
-    private CharacterController Controller;
-   
+    public CharacterController Controller;
+
     public Vector3 dashDirection;
-    public float dashDistance = 15;
+    public float dashDistance = 10;
     public float dashSpeed = 10;
+    public int dashesRemaining = 3;
+
 
     //member variables for movement
     public float speed = 5f;
@@ -17,72 +19,103 @@ public class Move : MonoBehaviour
     public float Velocity;                                      //you get it
     public float Force = 9.8f;
     public Vector3 moveVector;                                  //important variable to store 3 variable for 3 directions(xyz)
-  
-   
+
+    private void Start()
+    {
+
+        StartCoroutine(incremental());
+
+
+
+    }
+    IEnumerator incremental()
+    {
+        while (true)
+        {
+            //Wait for 30 seconds
+            yield return new WaitForSeconds(2);
+            if (dashesRemaining < 3)
+            {
+                //Increment Speed
+                incrementDashesRemaining();
+            }
+        }
+
+    }
+
+    void incrementDashesRemaining()
+    {
+       dashesRemaining += 1;
+        print("New Dash Count is " + dashesRemaining);
+    }
     void Update()
     {
         MovePlayer();
-        Debug.Log(Velocity);
-        if (Input.GetButtonDown("Fire2"))
+     
+        if (Input.GetButtonDown("Fire2") || Input.GetKeyDown(KeyCode.LeftShift) && dashesRemaining > 0)
         {
           
+                print("DAsh");
                 Dash();
+            dashesRemaining--;
+                
+            
             
         }
     }
     void Dash()
     {
+        
         dashDirection = transform.forward * dashSpeed;                          //get the forward vector and make it the direction(forward)         
         Controller.Move(Time.deltaTime * dashDistance * dashSpeed * dashDirection); //call the character move function and basically move it a big number
-
+        
+        print(dashesRemaining + " Remaining ");
     }
     /*
     void DashBackWards()
     {
         dashDirection = -transform.forward * dashSpeed;                                 
         Controller.Move(Time.deltaTime * dashDistance * dashSpeed * dashDirection);
-
     }
     */
     void Awake()                                                //get a reference to the character controller on script load
     {
-        Controller = GetComponent<CharacterController>(); 
+        Controller = GetComponent<CharacterController>();
     }
 
     void MovePlayer()
     {
-        
+
         //declare the move variable 
-        moveVector = new Vector3(Input.GetAxis("Horizontal"),0f,Input.GetAxis("Vertical"));
+        moveVector = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
 
         moveVector = transform.TransformDirection(moveVector);     //move from local to world space(go in the direction you are looking        
         moveVector *= speed * Time.deltaTime;                      //make it a constant speed
         Gravity();
         Controller.Move(moveVector);                               //move bro                      
-        
 
-    } 
 
-    void Gravity()                                  //add "artificial gravity" so that the controller is touching the ground
+    }
+
+     public void Gravity()                                  //add "artificial gravity" so that the controller is touching the ground
     {
 
         Velocity -= gravity * Time.deltaTime;      //start making the valocity negative 
 
-        
+
         Jump();                              //jump
 
         moveVector.y = Velocity * Time.deltaTime;   //when spacebar is pressed it launches you into the air, and gravity will bring you down
     }
-   
-        void Jump()
+
+     public void Jump()
+    {
+
+        if (Controller.isGrounded && Input.GetKeyDown(KeyCode.Space)) //if you press space bar you
         {
-
-            if (Controller.isGrounded && Input.GetKeyDown(KeyCode.Space)) //if you press space bar you
-            {
-                     Velocity = Force;                                         //jump
-            }
-
+            Velocity = Force;                                         //jump
         }
 
-} 
+    }
 
+}
